a88f52774f5fd5cd9768fa598719ef0fd11e9017
Daria <larina.d.s@gmail.com>
2023-03-12 20:37:52 +0300

    homework2

---
+++ HW2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine()!;
+// if (number.Length == 3){
+//     Console.WriteLine($"{number} -> {number[1]}");
+// }
+// else {
+// Console.WriteLine($"Введено неверное число");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine()!;
+// if (number.Length >= 3){
+//     Console.WriteLine($"{number} -> третья цифра - {number[2]}");
+// }
+// else {
+// Console.WriteLine($"{number} - третьей цифры нет");
+// }
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number <= 5){
+//     Console.WriteLine($"{number} -> будни");
+// }
+// if (number == 6 || number == 7){
+//     Console.WriteLine($"{number} -> выходной");
+// }
+// if (number > 7){
+// Console.WriteLine($"{number} - такого дня недели нет");
+// }
