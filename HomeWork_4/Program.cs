3805fc61cb23d79f4f33e555ddb8457a08eddfbd
Daria <larina.d.s@gmail.com>
2023-03-19 22:54:43 +0300

    homework_4

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// Console.Write("Введите число: ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите степень: ");
+// int B = int.Parse(Console.ReadLine()!);
+// int n = 1;
+// int C = 1;
+// while (n <= B )
+// {
+//    C = C * A;
+//     n++;
+// }
+// Console.WriteLine($"{C}");
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//  Console.Write("Введите число: ");
+// string num = Console.ReadLine()!;
+// int S = 0;
+// for ( int i = 0; i < num.Length; i++)
+// {
+//     S += Convert.ToInt32(num[i].ToString());
+    
+// }
+//     Console.WriteLine($"{num} -> {S}");
+        
+//   Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//   int[] array = new int [8];
+//   Random rand = new Random();
+// for (int i = 0; i < array.Length; i++)
+//    array[i] = rand.Next();
+//    Console.WriteLine("{0}", string.Join(", ", array));
